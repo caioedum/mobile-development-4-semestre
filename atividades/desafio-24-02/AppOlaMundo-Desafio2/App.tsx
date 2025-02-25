86178031cdb91cca5ec68cea5dcc434e2afc09af
Caio Eduardo <caio3martins@gmail.com>
2025-02-24 21:14:34 -0300

    feat: add today's class and challenges

---
+++ atividades/desafio-24-02/AppOlaMundo-Desafio2/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bem-vindo ao React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: '#3e9afc',
+    fontWeight: 'bold'
+  }
+});
